395c39cf993109e3fb10423cda733bca7ded689f
harunsefainan <harunsefa.inan@gmail.com>
2024-05-06 00:13:52 +0300

    create user entity

---
+++ src/main/java/com/harunsefainan/carrentalbackend/entity/User.java
@@ -0,0 +1,22 @@
+package com.harunsefainan.carrentalbackend.entity;
+
+import com.harunsefainan.carrentalbackend.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private UserRole userRole;
+
+}
