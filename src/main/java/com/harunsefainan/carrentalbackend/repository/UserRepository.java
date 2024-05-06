4b8a1917de7f704944b3b34dd2ed04af74753b03
harunsefainan <harunsefa.inan@gmail.com>
2024-05-06 12:06:54 +0300

    create findFirstByEmail method

--- src/main/java/com/harunsefainan/carrentalbackend/repository/UserRepository.java
+++ src/main/java/com/harunsefainan/carrentalbackend/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.harunsefainan.carrentalbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findFirstByEmail(String email);
 }
