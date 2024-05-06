6cd8b5208c0a4a5b0720f33bc1db261911ef9400
harunsefainan <harunsefa.inan@gmail.com>
2024-05-06 12:06:31 +0300

    update UserDto

--- src/main/java/com/harunsefainan/carrentalbackend/dto/UserDto.java
+++ src/main/java/com/harunsefainan/carrentalbackend/dto/UserDto.java
@@ -1,7 +1,16 @@
 package com.harunsefainan.carrentalbackend.dto;
 
+import com.harunsefainan.carrentalbackend.enums.UserRole;
 import lombok.Data;
 
 @Data
 public class UserDto {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private UserRole userRole;
 }
