82d9e0121a4029a26a43ee95cce77d69d663bb1d
harunsefainan <harunsefa.inan@gmail.com>
2024-05-06 12:04:55 +0300

    create customer and hasCustomerWithEmail

--- src/main/java/com/harunsefainan/carrentalbackend/services/auth/AuthService.java
+++ src/main/java/com/harunsefainan/carrentalbackend/services/auth/AuthService.java
@@ -1,4 +1,10 @@
 package com.harunsefainan.carrentalbackend.services.auth;
 
+import com.harunsefainan.carrentalbackend.dto.SignupRequest;
+import com.harunsefainan.carrentalbackend.dto.UserDto;
+
 public interface AuthService {
+    UserDto createCustomer(SignupRequest signupRequest);
+
+    boolean hasCustomerWithEmail(String email);
 }
