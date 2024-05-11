bdb7558bb6c7a061b1f94b7d7ab948e0bff4854c
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 23:55:36 +0300

    getAllCars method added

--- src/main/java/com/harunsefainan/carrentalbackend/services/admin/AdminServiceImpl.java
+++ src/main/java/com/harunsefainan/carrentalbackend/services/admin/AdminServiceImpl.java
@@ -6,6 +6,9 @@ import com.harunsefainan.carrentalbackend.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +35,10 @@ public class AdminServiceImpl implements AdminService {
             return false;
         }
 
+    }
+
+    @Override
+    public List<CarDto> getAllCars() {
+        return carRepository.findAll().stream().map(Car::getCarDto).collect(Collectors.toList());
     }
 }
