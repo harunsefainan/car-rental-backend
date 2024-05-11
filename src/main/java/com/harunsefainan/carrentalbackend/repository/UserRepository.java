517848f92c350be28ba0935c886cc3035ca05149
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 21:42:53 +0300

    findBtUerRole method added

--- src/main/java/com/harunsefainan/carrentalbackend/repository/UserRepository.java
+++ src/main/java/com/harunsefainan/carrentalbackend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.harunsefainan.carrentalbackend.repository;
 
 import com.harunsefainan.carrentalbackend.entity.User;
+import com.harunsefainan.carrentalbackend.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findFirstByEmail(String email);
+
+    User findByUserRole(UserRole userRole);
 }
