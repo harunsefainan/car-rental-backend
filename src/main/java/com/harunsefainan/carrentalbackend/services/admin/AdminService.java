960820a65cdb21833314a664f54419cf7e2b81af
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 23:55:27 +0300

    getAllCars method added

--- src/main/java/com/harunsefainan/carrentalbackend/services/admin/AdminService.java
+++ src/main/java/com/harunsefainan/carrentalbackend/services/admin/AdminService.java
@@ -3,9 +3,12 @@ package com.harunsefainan.carrentalbackend.services.admin;
 import com.harunsefainan.carrentalbackend.dto.CarDto;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AdminService {
 
     boolean postCar(CarDto carDto) throws IOException;
+
+    List<CarDto> getAllCars();
 
 }
