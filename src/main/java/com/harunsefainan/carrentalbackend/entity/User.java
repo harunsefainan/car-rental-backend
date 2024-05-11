5181d2057089fc03e20008c4829c687d45eb62c3
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 21:03:18 +0300

    update User.java

--- src/main/java/com/harunsefainan/carrentalbackend/entity/User.java
+++ src/main/java/com/harunsefainan/carrentalbackend/entity/User.java
@@ -3,11 +3,17 @@ package com.harunsefainan.carrentalbackend.entity;
 import com.harunsefainan.carrentalbackend.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +27,33 @@ public class User {
 
     private UserRole userRole;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
