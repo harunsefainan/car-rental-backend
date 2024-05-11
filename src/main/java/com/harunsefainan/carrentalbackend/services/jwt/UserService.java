02aa1dc00319737242e4454756dee7e09f003987
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 21:02:25 +0300

    create UserService

---
+++ src/main/java/com/harunsefainan/carrentalbackend/services/jwt/UserService.java
@@ -0,0 +1,9 @@
+package com.harunsefainan.carrentalbackend.services.jwt;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+
+    UserDetailsService userDetailsService();
+}
