81f2f04a8076fe36aa187a8f0930b8f851266964
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 21:42:32 +0300

    createAdminAccount method added

--- src/main/java/com/harunsefainan/carrentalbackend/services/auth/AuthServiceImpl.java
+++ src/main/java/com/harunsefainan/carrentalbackend/services/auth/AuthServiceImpl.java
@@ -5,7 +5,9 @@ import com.harunsefainan.carrentalbackend.dto.UserDto;
 import com.harunsefainan.carrentalbackend.entity.User;
 import com.harunsefainan.carrentalbackend.enums.UserRole;
 import com.harunsefainan.carrentalbackend.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +16,26 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
 
+    @PostConstruct
+    public void createAdminAccount() {
+        User adminAccount = userRepository.findByUserRole(UserRole.ADMIN);
+        if (adminAccount == null) {
+            User newAdminAccount = new User();
+            newAdminAccount.setName("Admin");
+            newAdminAccount.setEmail("admin@test.com");
+            newAdminAccount.setPassword(new BCryptPasswordEncoder().encode("admin"));
+            newAdminAccount.setUserRole(UserRole.ADMIN);
+            userRepository.save(newAdminAccount);
+            System.out.println("Admin account created successfully");
+        }
+    }
+
     @Override
     public UserDto createCustomer(SignupRequest signupRequest) {
-        User user =new User();
+        User user = new User();
         user.setName(signupRequest.getName());
         user.setEmail(signupRequest.getEmail());
-        user.setPassword(signupRequest.getPassword());
+        user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
         user.setUserRole(UserRole.CUSTOMER);
         User createdUser = userRepository.save(user);
         UserDto userDto = new UserDto();
