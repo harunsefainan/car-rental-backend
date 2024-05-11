98e5cd5e0809f3bb76e205e86cc93233a68b0f6b
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 23:39:00 +0300

    created AdminController

---
+++ src/main/java/com/harunsefainan/carrentalbackend/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.harunsefainan.carrentalbackend.controller;
+
+import com.harunsefainan.carrentalbackend.dto.CarDto;
+import com.harunsefainan.carrentalbackend.services.admin.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @PostMapping("/car")
+    public ResponseEntity<?> postCar(@ModelAttribute CarDto carDto) throws IOException {
+        boolean success = adminService.postCar(carDto);
+        if (success) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+}
