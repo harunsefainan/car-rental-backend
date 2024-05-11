27349e1aa1cda43fecffb318da86264ecae515d8
harunsefainan <harunsefa.inan@gmail.com>
2024-05-11 23:55:51 +0300

    getCarDto method added

--- src/main/java/com/harunsefainan/carrentalbackend/entity/Car.java
+++ src/main/java/com/harunsefainan/carrentalbackend/entity/Car.java
@@ -1,5 +1,6 @@
 package com.harunsefainan.carrentalbackend.entity;
 
+import com.harunsefainan.carrentalbackend.dto.CarDto;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -33,5 +34,19 @@ public class Car {
     @Column(columnDefinition = "longblob")
     private byte[] image;
 
+    public CarDto getCarDto() {
+        CarDto carDto = new CarDto();
+        carDto.setId(id);
+        carDto.setBrand(brand);
+        carDto.setColor(color);
+        carDto.setName(name);
+        carDto.setType(type);
+        carDto.setTransmission(transmission);
+        carDto.setDescription(description);
+        carDto.setPrice(price);
+        carDto.setYear(year);
+        carDto.setReturnedImage(image);
+        return carDto;
+    }
 }
 
