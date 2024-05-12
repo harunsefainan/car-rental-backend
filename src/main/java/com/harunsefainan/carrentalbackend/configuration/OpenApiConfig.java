b2579d73d6a405f76b5dc603748cd9bf5836342b
harunsefainan <harunsefa.inan@gmail.com>
2024-05-12 16:09:04 +0300

    created OpenApiConfig

---
+++ src/main/java/com/harunsefainan/carrentalbackend/configuration/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.harunsefainan.carrentalbackend.configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "Bearer"
+)
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI configureOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Your API Title")
+                        .description("Your API Description")
+                        .version("1.0"));
+    }
+}
