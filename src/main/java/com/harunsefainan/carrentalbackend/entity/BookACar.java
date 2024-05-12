605335e9c52f52becfddea93da341c059b434730
harunsefainan <harunsefa.inan@gmail.com>
2024-05-12 19:10:22 +0300

    created BookACar

---
+++ src/main/java/com/harunsefainan/carrentalbackend/entity/BookACar.java
@@ -0,0 +1,42 @@
+package com.harunsefainan.carrentalbackend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.harunsefainan.carrentalbackend.enums.BookCarStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "bookacar")
+public class BookACar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date fromDate;
+
+    private Date toDate;
+
+    private Long days;
+
+    private Long price;
+
+    private BookCarStatus bookCarStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "car_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Car car;
+}
