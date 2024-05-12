ad5ef4f1ddd7c2ef7f0ac61a0aebbfc9c441dfb5
harunsefainan <harunsefa.inan@gmail.com>
2024-05-12 19:56:05 +0300

    findAllByUserId mehod  added

--- src/main/java/com/harunsefainan/carrentalbackend/repository/BookACarRepository.java
+++ src/main/java/com/harunsefainan/carrentalbackend/repository/BookACarRepository.java
@@ -1,9 +1,13 @@
 package com.harunsefainan.carrentalbackend.repository;
 
+import com.harunsefainan.carrentalbackend.dto.BookACarDto;
 import com.harunsefainan.carrentalbackend.entity.BookACar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookACarRepository extends JpaRepository<BookACar, Long> {
+    List<BookACar> findAllByUserId(Long userId);
 }
